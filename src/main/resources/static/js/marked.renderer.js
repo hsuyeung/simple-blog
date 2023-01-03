c8a0ae9bdc11edeffa05184ffb215cb4080566c0
hsuyeung <hsuyeung.com@gmail.com>
2023-01-03 14:29:10 +0800

    refactor: marked 渲染图片、链接时去掉 title 属性

--- src/main/resources/static/js/marked.renderer.js
+++ src/main/resources/static/js/marked.renderer.js
@@ -1,10 +1,10 @@
 /* 对 marked.js 的渲染规则改写 */
 const renderer = {
   image(href, title, text) {
-    return `<figure><img class='zoom-img' data-src='${href}' src='/img/loading.gif' title='${href}' alt='${text}'/><figcaption>${text}</figcaption></figure>`
+    return `<figure><img class='zoom-img' data-src='${href}' src='/img/loading.gif' alt='${text}'/><figcaption>${text}</figcaption></figure>`
   },
   link(href, title, text) {
-    return `<a href='${href}' target='_blank' title='${href}' rel='noopener noreferrer nofollow'>${text}</a>`
+    return `<a href='${href}' target='_blank' rel='noopener noreferrer nofollow'>${text}</a>`
   }
 }
 
